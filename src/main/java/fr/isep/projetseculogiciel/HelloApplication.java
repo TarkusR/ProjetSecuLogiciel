12259d9d7ed436f3a34c605e010b72ea16eb0f4c
nigris <92745772+nigris@users.noreply.github.com>
2024-01-17 20:21:35 +0100

    Update HelloApplication.java
    add stylesheet link and change the scene .

--- src/main/java/fr/isep/projetseculogiciel/HelloApplication.java
+++ src/main/java/fr/isep/projetseculogiciel/HelloApplication.java
@@ -1,7 +1,10 @@
 package fr.isep.projetseculogiciel;
 
+// package com.example.demo;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,8 +14,14 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root, 1200, 800);
+
+        // Ajouter la feuille de style à la scène
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+        stage.setTitle("Isep project");
         stage.setScene(scene);
         stage.show();
     }
