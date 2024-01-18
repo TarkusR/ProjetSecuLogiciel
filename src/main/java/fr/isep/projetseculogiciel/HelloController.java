88631b332c5e5ac65630d3e4d571e1d2297d2bd4
nigris <92745772+nigris@users.noreply.github.com>
2024-01-18 21:38:45 +0100

    Update HelloController.java
    add feature : for each box select a new instance

--- src/main/java/fr/isep/projetseculogiciel/HelloController.java
+++ src/main/java/fr/isep/projetseculogiciel/HelloController.java
@@ -1,5 +1,4 @@
 package fr.isep.projetseculogiciel;
-
 // package com.example.demo;
 
 import javafx.fxml.FXML;
@@ -9,7 +8,6 @@ import javafx.scene.control.TextField;
 import java.util.ArrayList;
 import java.util.List;
 
-//todo : test if the value of checkbox are correctly selected
 public class HelloController {
 
     @FXML
@@ -36,7 +34,7 @@ public class HelloController {
     @FXML
     private void startAttacks() {
         // Récupérer la valeur du TextField
-        String url = customTextField.getText();
+        String textFieldValue = customTextField.getText();
 
         // Créer une liste de CheckBox
         List<CheckBox> checkBoxList = new ArrayList<>();
@@ -52,11 +50,34 @@ public class HelloController {
             // Récupérer l'état de la CheckBox
             boolean isSelected = checkBox.isSelected();
 
-            //  TODO  : START A NE INSTANCE OF EACH FEATURE FOR EACH CHECKBOX SELECTED 
+            // Si la CheckBox est sélectionnée, effectuer l'action correspondante
             if (isSelected) {
-                // Instancier la classe F6 en lui passant l'URL (ou d'autres informations) en paramètre
-                F6 f6Automation = new F6(url);
-                f6Automation.performWebAutomation();
+                if (checkBox == f1) {
+                    // TODO
+//                    F1Controller bruteForce = new F1Controller(textFieldValue);
+                    System.out.println("F1 selected. Perform action for F1.");
+                } else if (checkBox == f2) {
+                    // TODO
+//                    F2Controller sqlInjection = new F2Controller(textFieldValue);
+                    System.out.println("F2 selected. Perform action for F2.");
+                } else if (checkBox == f3) {
+                    F3Controller xssAttack = new F3Controller(textFieldValue);
+                    xssAttack.performWebAutomation();
+                    // Action spécifique pour f3
+                    System.out.println("F3 selected. Perform action for F3.");
+                } else if (checkBox == f4) {
+                    // TODO
+//                    F4Controller f4 = new F4Controller(textFieldValue);
+                    System.out.println("F4 selected. Perform action for F4.");
+                } else if (checkBox == f5) {
+//                    F5Controller f5 = new F5Controller(textFieldValue);
+                    System.out.println("F5 selected. Perform action for F5.");
+                } else if (checkBox == f6) {
+//                    F6Controller f6 = new F6Controller(textFieldValue);
+
+                    System.out.println("F6 selected. Perform action for F6.");
+                }
+                // Ajoutez d'autres blocs else if si nécessaire pour les CheckBox supplémentaires
             }
         }
     }
